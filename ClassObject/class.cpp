a62372d5c378806d2bd68e77f118e44a10476fb5
root <you@example.com>
2024-02-29 15:02:24 +0800

    [修改] 类的构造函数

--- ClassObject/class.cpp
+++ ClassObject/class.cpp
@@ -1,25 +1,58 @@
 #include <iostream>
+#include <string>
 
 /* 通过 关键字 class定义类 */
 class Student
 {
 public:
-    const char *name;
-    int age;
+    // Student()
+    // {
+    //     std ::cout << "构造" << std ::endl;
+    // }
 
-    /*定义函数 */
-    void printS()
+    Student(int age, const std ::string name)
     {
-        printf(" name :%s and age  %d\n", name, age);
+        m_age = age;
+        m_name = name;
+        std ::cout << "有参构造" << std ::endl;
     }
+
+    /* 类内声明 */
+    void Setage(int age);
+     int getAge();
+    void setname(const std::string &name); 
+    std ::string getName();
+
+private:
+    int m_age;
+    std ::string m_name;
 };
+
+/* 类外定义 */
+void Student::Setage(int age)
+{
+    m_age = age;
+}
+int Student ::getAge()
+{
+    return m_age;
+}
+void Student::setname(const std::string &name)
+{
+    m_name = name;
+}
+std ::string Student ::getName()
+{
+    return m_name;
+}
 
 int main()
 {
-    Student stu;
-    stu.name = "张三";
-    stu.age = 19;
-    stu.printS();
+    Student stu(10, "zhansan");
+
+    // stu.Setage(10);
+    // stu.setname("zhangsan");
+    std ::cout  << "age:" << stu.getAge()<< ",name:" << stu. getName()  << std ::endl;
 
     return 0;
 }
